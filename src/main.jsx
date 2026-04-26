f90f9bd9bf2c9fa332d8ce6766b7ea1d96478517
yawnbo <main@yawnbo.com>
2026-04-25 18:42:01 -0700

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App.jsx';
+import { inject } from "@vercel/analytics"
 
+inject()
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
